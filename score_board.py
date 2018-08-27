e0963b6b208ef4617ad8b2528010892284bdd97d
yjqiang <13307130285@fudan.edu.cn>
2018-08-27 21:51:31 +0800

    减少texture不必要传入

--- score_board.py
+++ score_board.py
@@ -9,6 +9,9 @@ class ScoreBoards(scene.Node):
         self.sprites = utils.load_sprite_sheet('numbers.png', 12, 1)[:10]
         size = self.sprites[0][1]
         self.score_boards = [ScoreBoard(size, scene.Point(i * size.x, 0)) for i in range(5)]
+        self.score_nums = [0, 0, 0, 0, 0]
+        for i in range(4, -1, -1):
+            self.score_boards[i].texture = self.sprites[0][0]
         for score_board in self.score_boards:
             self.add_child(score_board)
                     
@@ -19,8 +22,10 @@ class ScoreBoards(scene.Node):
             # i 个十百千万 num对应数字
             num = score % 10
             score = int(score / 10)
-            self.score_boards[i].texture = self.sprites[num][0]
-
+            if self.score_nums[i] != num:
+                self.score_boards[i].texture = self.sprites[num][0]
+                self.score_nums[i] = num
+        
                         
 class ScoreBoard(scene.SpriteNode):
     def __init__(self, size, position):
