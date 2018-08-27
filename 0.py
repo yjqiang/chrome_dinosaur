b4d1358c43bbf79cd4c4ae4f05e57ff7228f7682
yjqiang <13307130285@fudan.edu.cn>
2018-08-27 19:28:59 +0800

    小修改

--- 0.py
+++ 0.py
@@ -7,12 +7,8 @@ class MyScene (scene.Scene):
     def setup(self):
         self.background_color = 'white'
         
-        # self.dino.position.x = (self.size.w / 1)
-        # self.dino.position.y = (self.size.h * 0.98)
-        # self.dino.position = (self.size.w / 15, self.size.h * 0.1)
-        
         self.dino = Dino()
-        # 这个sb中心点是坐标
+        # 这个sb pythonista作者设置的texture中心点作为坐标，而且与opencv坐标不同，|_这种坐标
         self.add_child(self.dino)
            
         self.grounds = Grounds()
@@ -23,6 +19,8 @@ class MyScene (scene.Scene):
         
         self.cactuses = Cactuses(self.size.x)
         self.add_child(self.cactuses)
+        
+        self.middle_x = self.size.x / 2
         
     def check_collision(self):
         if self.cactuses.check_collision(self.dino.left_buttom_coord, self.dino.size):
@@ -39,8 +37,7 @@ class MyScene (scene.Scene):
         self.check_collision()
             
     def touch_began(self, touch):
-        middle_x = self.size.x / 2
-        if touch.location.x < middle_x:
+        if touch.location.x < self.middle_x:
             self.dino.start_jump()
         else:
             self.dino.start_duck()
