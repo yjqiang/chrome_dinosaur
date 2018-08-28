f8942505ee59ec43a52e4a2caa2df98484a6b475
yjqiang <13307130285@fudan.edu.cn>
2018-08-28 08:10:32 +0800

    scoreboard封装texture更改

--- score_board.py
+++ score_board.py
@@ -6,12 +6,13 @@ class ScoreBoards(scene.Node):
     def __init__(self):
         self.position = scene.Point(100, 100)
         self.score = 0
-        self.sprites = utils.load_sprite_sheet('numbers.png', 12, 1)[:10]
-        size = self.sprites[0][1]
-        self.score_boards = [ScoreBoard(size, scene.Point(i * size.x, 0)) for i in range(5)]
+        sprites = utils.load_sprite_sheet('numbers.png', 12, 1)[:10]
+        size = sprites[0][1]
+        textures = [sprite[0] for sprite in sprites]
+        self.score_boards = [ScoreBoard(textures, size, scene.Point(i * size.x, 0)) for i in range(5)]
         self.score_nums = [0, 0, 0, 0, 0]
         for i in range(4, -1, -1):
-            self.score_boards[i].texture = self.sprites[0][0]
+            self.score_boards[i].update(0)
         for score_board in self.score_boards:
             self.add_child(score_board)
                     
@@ -23,14 +24,14 @@ class ScoreBoards(scene.Node):
             num = score % 10
             score = int(score / 10)
             if self.score_nums[i] != num:
-                self.score_boards[i].texture = self.sprites[num][0]
+                self.score_boards[i].update(num)                
                 self.score_nums[i] = num
         
                         
 class ScoreBoard(scene.SpriteNode):
-    def __init__(self, size, position):
-        # self.texture = texture
+    def __init__(self, textures, size, position):
+        self.textures = textures
         self.position = position + size / 2
             
-    def update(self, texture):
-        self.texture = texture
+    def update(self, num):
+        self.texture = self.textures[num]
